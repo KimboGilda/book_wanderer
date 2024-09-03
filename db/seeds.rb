0e56ac035f73699f08a98f8d67f4168b190f1a6f
nikos bakogiannis <nbakojohn13@gmail.com>
2024-09-03 17:11:20 +0200

    fix the issue witj the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,15 @@ require 'httparty'
 require 'faker'
 
 puts "Destroying all data"
-# Booking.destroy_all
-# Review.destroy_all
+ReadBook.destroy_all
+UserLibrary.destroy_all
+Review.destroy_all
 Book.destroy_all
 User.destroy_all
 puts "All data destroyed"
 
 def get_books(books)
   url = "https://www.googleapis.com/books/v1/volumes?q=#{books}&key=#{ENV['API_KEY']}&langRestrict=en"
-
-
 
   response = HTTParty.get(url)
   if response.success?
@@ -55,12 +54,26 @@ random_books.each do |data|
   genre = data['volumeInfo']['categories']&.join(', ')
   cover_image_url = data['volumeInfo']['imageLinks']&.dig('thumbnail')
   short_summary = summary
-  Book.create!(title: title, author: author, publication_year: publication_year, summary: summary, short_summary: short_summary, genre: genre, cover_image_url:cover_image_url)
-  # if title && author
-  #   Book.create(title: title, author: author, publication_year: publication_year, summary: summary, short_summary: short_summary, genre: genre, cover_image_url:cover_image_url)
-  # end
+  if author == nil
+    author = Faker::Book.author
+  end
+  if genre == nil
+    genre = Faker::Book.genre
+  end
+  if title == nil
+    title = Faker::Book.title
+  end
+  if summary == nil
+    summary = Faker::Lorem.paragraphs(number: 2).join("\n")
+  end
+
+  unless Book.exists?(title: title, author: author)
+    Book.create!(title: title, author: author, publication_year: publication_year, summary: summary, short_summary: short_summary, genre: genre, cover_image_url:cover_image_url)
+  end
+
+
 end
-puts "#{random_books.size} random books added"
+puts "#{random_books.size} random books added *(change in seeds if you need)"
 
 
 
@@ -71,46 +84,64 @@ puts "#{random_books.size} random books added"
 
 user_01 = User.create!(
   email: "mariasarapova@gmail.com",
-  password: "wimbledon13",
-  first_name: 'Maria',
-  last_name: "Sarapova",
-  profile_picture: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQF1OLdIQ262byvZOwohTsqrBedYYMrwunw7BPelkg3X4-BM7ZW"
+  password: "wimbledon13"
 )
 
 user_02 = User.create!(
   email: "emmagoldman@gmail.com",
-  password: "aa13uk",
-  first_name: 'Emma',
-  last_name: "Goldman",
-  profile_picture: "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcR-YjPx0YjDeF6-MEyDHOSr8ZQuhGQaHCdTxy1JXdoxP0W2Pg-BiXotcwC5QPdt_bWowPugRGHjo7MYshY4bs6irlM-WHoXdTIE4_T4sZPhzegThYeKNE6sQka_1dTkWP4MqFCpR5P8"
+  password: "aa13uk"
 )
 
 user_03 = User.create!(
   email: "oliviamartin@gmail.com",
-  password: "olivia2024",
-  first_name: 'Olivia',
-  last_name: "Martin",
-  profile_picture: "https://cdn.canvasrebel.com/wp-content/uploads/2023/07/c-PersonalOliviaMartin__IMG2706_1688392668767.jpeg"
+  password: "olivia2024"
 )
 
 user_04 = User.create!(
   email: "jameson@gmail.com",
-  password: "james23",
-  first_name: 'James',
-  last_name: "On",
+  password: "james23"
 )
 
 
+
 users = [user_01, user_02, user_03, user_04]
+
+
 # READ BOOKS
 
 
-random_read_book = all_books.sample(5)
+random_read_book = Book.all.sample(5)
 random_read_book.each do |data|
+  rand(1..5).times do
+    ReadBook.create!(user_id: users.sample.id, book_id: data.id)
 
-  ReadBook.create!(user_id: )
-  # if title && author
-  #   Book.create(title: title, author: author, publication_year: publication_year, summary: summary, short_summary: short_summary, genre: genre, cover_image_url:cover_image_url)
-  # end
+  end
 end
-puts "#{random_books.size} random books added"
+puts "Read books added"
+
+
+
+# REVIEWS   -----------------------------------------------------
+
+content = Faker::Lorem.paragraph
+read_book = ReadBook.all
+
+read_book.each do |data|
+  Review.create!(read_book_id: read_book.sample.id, content: content)
+
+end
+puts "Reviews added"
+
+
+
+# USER LIBRARY ------------------------------------------
+#
+random_book_in_library = Book.all.sample(5)
+random_book_in_library.each do |data|
+  rand(1..5).times do
+    UserLibrary.create!(user_id: users.sample.id, book_id: data.id)
+  end
+end
+puts "Book to USER LIBRARY added"
+
+puts 'End for now, mb some changes later'
