93b6f55a4ea2c79aef1255be856aaa9c544249c1
nikos bakogiannis <nbakojohn13@gmail.com>
2024-09-04 14:23:34 +0200

    other readers liked feature implemented

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,5 +9,15 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+
+    # Find users who have read the current book
+    # pluck is a built in ruby method that shows as more than one value - it's like distinct in SQL
+    user_ids = ReadBook.where(book_id: @book.id).pluck(:user_id)
+    
+    # Find other books that these users have read (NOT includnig the current book)
+    @other_books = Book.joins(:read_books)
+                       .where(read_books: { user_id: user_ids })
+                       .where.not(id: @book.id)
+                       .distinct
   end
 end
