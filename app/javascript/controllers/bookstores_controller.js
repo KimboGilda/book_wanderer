21b8740865bcee18b1d1190fdc1148c253f966e4
nikos bakogiannis <nbakojohn13@gmail.com>
2024-09-04 13:43:40 +0200

    add stimulus functionality for displaying the map

--- app/javascript/controllers/bookstores_controller.js
+++ app/javascript/controllers/bookstores_controller.js
@@ -1,11 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["map"];
   connect() {
     console.log("Hello from our first Stimulus controller");
   }
 
   toggle_map(event) {
-    console.log("hello from the controller");
+    if (this.mapTarget.style.display === "none") {
+      this.mapTarget.style.display = "block";
+    } else {
+      this.mapTarget.style.display = "none";
+    }
   }
 }
