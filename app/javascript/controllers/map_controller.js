a05d6c94fe22c00f28e3f7ac7dbfcd5767816430
nikos bakogiannis <nbakojohn13@gmail.com>
2024-09-06 12:52:42 +0200

    add markers

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -16,5 +16,22 @@ export default class extends Controller {
       zoom: 12,
       center: [23.7204, 37.9705]
     });
+
+    this.#addMarkersToMap();
+    this.#fitMapToMarkers();
+  }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(this.map);
+    });
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach((marker) =>
+      bounds.extend([marker.lng, marker.lat])
+    );
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
 }
