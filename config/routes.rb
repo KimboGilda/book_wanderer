deb3cddefd160176a7bb8259578757f3edf76c11
nikos bakogiannis <nbakojohn13@gmail.com>
2024-09-09 16:06:37 +0200

    add some stuff to the pages contriller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get 'random_books', to: 'pages#random_books', defaults: { format: :json }
+  get 'our_selection', to: 'pages#our_selection', defaults: { format: :json }
 
   get "up" => "rails/health#show", as: :rails_health_check
 
